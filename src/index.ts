ed77e9d82c10fc5dd39b40169a7c413017fd3a7b
dsaglam94 <d.saglam@outlook.fr>
2023-01-01 22:22:23 +0300

    creates and implements jwt

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,17 @@ import {
 } from "apollo-server-core";
 import { resolvers } from "./resolvers";
 import connectDb from "./utils/mongo";
+import Context from "./types/context";
+import { verifyJwt } from "./utils/jwt";
+import { User } from "./schema/user.schema";
 
 async function bootstrap() {
   const schema = await buildSchema({
     resolvers,
     // authCheckers
+    validate: {
+      forbidUnknownValues: false,
+    },
   });
 
   const app = express();
@@ -24,9 +30,16 @@ async function bootstrap() {
 
   const server = new ApolloServer({
     schema,
-    context: (ctx) => {
-      console.log("ctx:", ctx);
-      return ctx;
+    context: (ctx: Context) => {
+      const context = ctx;
+
+      if (ctx.req.cookies.accessToken) {
+        const user = verifyJwt<User>(ctx.req.cookies.accessToken);
+
+        context.user = user;
+      }
+
+      return context;
     },
     plugins: [
       process.env.NODE_ENV === "production"
