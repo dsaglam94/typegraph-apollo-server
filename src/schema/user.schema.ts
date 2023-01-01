622f1e49e2bf2f698afd8ac0148927a5fc22f569
dsaglam94 <d.saglam@outlook.fr>
2023-01-01 20:13:11 +0300

    updates user schema to get user model

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -1,5 +1,6 @@
-import { prop } from "@typegoose/typegoose";
-import { Field, ObjectType } from "type-graphql";
+import { getModelForClass, prop } from "@typegoose/typegoose";
+import { IsEmail, MaxLength, MinLength } from "class-validator";
+import { Field, InputType, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class User {
@@ -15,5 +16,26 @@ export class User {
   email: string;
 
   @prop({ required: true })
+  password: string;
+}
+
+export const UserModel = getModelForClass(User);
+
+@InputType()
+export class CreateUserInput {
+  @Field((_type) => String)
+  name: string;
+
+  @IsEmail()
+  @Field((_type) => String)
+  email: string;
+
+  @MinLength(6, {
+    message: "Password must be at least 6 characters long",
+  })
+  @MaxLength(50, {
+    message: "Password must not be longer than 50 characters",
+  })
+  @Field((_type) => String)
   password: string;
 }
