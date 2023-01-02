eb6e90297fc0ff05162cbe44417e184b9589eccc
dsaglam94 <d.saglam@outlook.fr>
2023-01-02 09:53:17 +0300

    changing name

--- src/resolvers/user.resolver.ts
+++ src/resolvers/user.resolver.ts
@@ -3,23 +3,23 @@ import { CreateUserInput, LoginInput, User } from "../schema/user.schema";
 import UserService from "../service/user.service";
 import Context from "../types/context";
 
-@Resolver()
+@Resolver((_of) => User)
 export default class UserResolver {
   constructor(private userService: UserService) {
     this.userService = new UserService();
   }
 
-  @Mutation((_of) => User)
+  @Mutation((_returns) => User)
   createUser(@Arg("input") input: CreateUserInput) {
     return this.userService.createUser(input);
   }
 
-  @Mutation((_of) => String) // Returns JWT
+  @Mutation((_returns) => String) // Returns JWT
   login(@Arg("input") input: LoginInput, @Ctx() context: Context) {
     return this.userService.login(input, context);
   }
 
-  @Query((_of) => User, { nullable: true })
+  @Query((_returns) => User, { nullable: true })
   me(@Ctx() context: Context) {
     return context.user;
   }
